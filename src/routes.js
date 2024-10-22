bcbd8fa479ae21b1a83c49f3445d1089d2f4e7f2
ayndri <dewinurayundari.dna@gmail.com>
2024-10-22 09:26:56 +0700

    fix bugs routes

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,48 @@
-/* eslint-disable indent *//* eslint-disable linebreak-style */
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+/* eslint-disable indent */ /* eslint-disable linebreak-style */
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: () => {
-            addNoteHandler();
-        },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: (request, h) => {
+      return addNoteHandler(request, h); // Return the handler's result
     },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: () => {
-            getAllNotesHandler();
-        },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: (request, h) => {
+      return getAllNotesHandler(request, h); // Return the handler's result
     },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: () => {
-            getNoteByIdHandler();
-        },
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: (request, h) => {
+      return getNoteByIdHandler(request, h); // Return the handler's result
     },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: () => {
-            editNoteByIdHandler();
-        }
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: (request, h) => {
+      return editNoteByIdHandler(request, h); // Return the handler's result
     },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: () => {
-            deleteNoteByIdHandler();
-        },
-    }
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: (request, h) => {
+      return deleteNoteByIdHandler(request, h); // Return the handler's result
+    },
+  },
 ];
 
 module.exports = routes;
